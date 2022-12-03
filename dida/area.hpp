f56a53d4dca41abd01c871ded680383f8df7570b
Lieven van der Heide <lievenvanderheide@gmail.com>
2022-12-03 14:08:32 +0100

    Implemented intersection_area for convex polygons

--- dida/area.hpp
+++ dida/area.hpp
@@ -11,4 +11,11 @@ namespace dida
 /// @return The area of @c polygon.
 double area(ConvexPolygonView2 polygon);
 
-}
+/// Returns the area of the intersection of polygons @c a and @c b.
+///
+/// @param a The first polygon.
+/// @param b The second polygon.
+/// @return The area of the intersection of polygons @c a and @c b.
+double intersection_area(ConvexPolygonView2 a, ConvexPolygonView2 b);
+
+} // namespace dida
--- dida/tests/area_tests.cpp
+++ dida/tests/area_tests.cpp
@@ -11,4 +11,39 @@ TEST_CASE("area")
   CHECK(area(polygon) == Approx(60));
 }
 
+TEST_CASE("intersection_area(ConvexPolygonView2, ConvexPolygonView2)")
+{
+  SECTION("Disjoint")
+  {
+    ConvexPolygon2 a{{-1, 4}, {3, 3}, {2, 5}};
+    ConvexPolygon2 b{{0, 2}, {1, -2}, {1, 3}};
+    CHECK(intersection_area(a, b) == 0);
+  }
+
+  SECTION("A leftmost in B")
+  {
+    ConvexPolygon2 a{{-3, 3}, {3, -3}, {7, -1}, {9, 3}, {2, 5}};
+    ConvexPolygon2 b{{8, 5}, {-1, 6}, {-4, 3}, {-3, 0}, {9, -3}, {10, 2}};
+    ConvexPolygon2 intersection_polygon{{-3, 3}, {1, -1}, {5, -2}, {7, -1}, {9, 3}, {2, 5}};
+    CHECK(intersection_area(a, b) == area(intersection_polygon));
+  }
+
+  SECTION("B leftmost in A")
+  {
+    ConvexPolygon2 a{{9, 5}, {7, 6}, {1, 5}, {-3, 3}, {-2, -1}, {2, -2}, {8, 1}, {10, 3}};
+    ConvexPolygon2 b{{1, 8}, {-2, 2}, {-1, -1}, {1, -2}, {5, -3}, {7, 3}};
+    ConvexPolygon2 intersection_polygon{{0, -1.5}, {2, -2}, {6, 0},  {7, 3},  {4, 5.5},
+                                        {1, 5},    {-1, 4}, {-2, 2}, {-1, -1}};
+    CHECK(intersection_area(a, b) == area(intersection_polygon));
+  }
+
+  SECTION("Both leftmosts vertices outside other")
+  {
+    ConvexPolygon2 a{{-2, -1}, {3, -3}, {8, -3}, {5, 3}, {-2, 3}};
+    ConvexPolygon2 b{{-3, 1}, {7, -4}, {8, 0}, {0, 4}};
+    ConvexPolygon2 intersection_polygon{{-1, 3}, {-2, 2}, {-2, .5}, {5, -3}, {7.25, -3}, {7.5, -2}, {6, 1}, {2, 3}};
+    CHECK(intersection_area(a, b) == area(intersection_polygon));
+  }
+}
+
 } // namespace dida
