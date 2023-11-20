189543340dcd5b08622268b57ba8532fcfa9cd09
Lieven van der Heide <lievenvanderheide@gmail.com>
2023-11-21 00:22:47 +0100

    Removed leftover debugging related code

--- dida/detail/vertical_decomposition/divide_and_conquer_builder.cpp
+++ dida/detail/vertical_decomposition/divide_and_conquer_builder.cpp
@@ -10,7 +10,7 @@ bool validate_chain_decomposition(VerticesView vertices, const ChainDecompositio
 
 ChainDecomposition merge_chain_decompositions_rec(VerticesView vertices, NodePool& node_pool,
                                                   ChainDecomposition* chain_decompositions,
-                                                  size_t num_chain_decompositions, size_t base_index)
+                                                  size_t num_chain_decompositions)
 {
   if (num_chain_decompositions == 1)
   {
@@ -19,9 +19,9 @@ ChainDecomposition merge_chain_decompositions_rec(VerticesView vertices, NodePoo
   else
   {
     size_t mid = num_chain_decompositions / 2;
-    ChainDecomposition a = merge_chain_decompositions_rec(vertices, node_pool, chain_decompositions, mid, base_index);
-    ChainDecomposition b = merge_chain_decompositions_rec(vertices, node_pool, chain_decompositions + mid,
-                                                          num_chain_decompositions - mid, base_index + mid);
+    ChainDecomposition a = merge_chain_decompositions_rec(vertices, node_pool, chain_decompositions, mid);
+    ChainDecomposition b =
+        merge_chain_decompositions_rec(vertices, node_pool, chain_decompositions + mid, num_chain_decompositions - mid);
     return merge_chain_decompositions(vertices, node_pool, a, b);
   }
 }
@@ -35,7 +35,7 @@ Node* vertical_decomposition_with_divide_and_conquer_builder(VerticesView vertic
   std::vector<ChainDecomposition> chain_decompositions = vertical_decomposition_zigzag_phase(vertices, node_pool);
 
   ChainDecomposition merged =
-      merge_chain_decompositions_rec(vertices, node_pool, chain_decompositions.data(), chain_decompositions.size(), 0);
+      merge_chain_decompositions_rec(vertices, node_pool, chain_decompositions.data(), chain_decompositions.size());
   return merged.first_node;
 }
 
