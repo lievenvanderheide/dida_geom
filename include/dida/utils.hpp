076f08cb62c1211a58d588089ee9e568ae820e83
Lieven van der Heide <lievenvanderheide@gmail.com>
2023-11-30 01:48:47 +0100

    Removed per-param documentation

--- include/dida/utils.hpp
+++ include/dida/utils.hpp
@@ -7,84 +7,45 @@ namespace dida
 
 /// Returns an iterator to the previous element from @c it, while wrapping around if the beginning of the container is
 /// reached.
-///
-/// @tparam Container The container type.
-/// @tparam Iterator The iterator type.
-/// @param container The container.
-/// @param it The original iterator.
-/// @return An iterator to the previous element from @c it.
 template <class Container, class Iterator>
 Iterator prev_cyclic(Container& container, Iterator it);
 
 /// Returns an iterator to the next element from @c it, while wrapping around if the end of the container is reached.
-///
-/// @tparam Container The container type.
-/// @tparam Iterator The iterator type.
-/// @param container The container.
-/// @param it The original iterator.
-/// @return An iterator to the previous element from @c it.
 template <class Container, class Iterator>
 Iterator next_cyclic(Container& container, Iterator it);
 
 /// Returns the distance from @c a to @c b in a cyclically ordered set. The result is the number of times @c next_cyclic
 /// has to be called to go from @c a to @c b.
-///
-/// @tparam Container The container type.
-/// @tparam Iterator The iterator type.
-/// @param container The container.
-/// @param a The iterator of the element to start measuring the distance from.
-/// @param b The iterator of the element to which to measure the distance.
-/// @return The distance from @c a to @c b.
 template <class Container, class Iterator>
 size_t distance_cyclic(const Container& container, Iterator a, Iterator b);
 
 /// Returns the predecessor of @c a modulo @c m.
 ///
 /// @pre @c a should be in the range <tt>0 <= a < m</tt>.
-/// @tparam IntType The type of @c a and @c m.
-/// @param a The integer modulo @c m whose predecessor to compute.
-/// @param m The integer modulo which the result is computed.
-/// @return The predecessor of @c a modulo @c m.
 template <class IntType>
 IntType pred_modulo(IntType a, IntType m);
 
 /// Returns the successor of @c a modulo @c m.
 ///
 /// @pre @c a should be in the range <tt>0 <= a < m</tt>.
-/// @tparam IntType The type of @c a and @c m.
-/// @param a The integer modulo @c m whose successor to compute.
-/// @param m The integer modulo which the result is computed.
-/// @return The successor of @c a modulo @c m.
 template <class IntType>
 IntType succ_modulo(IntType a, IntType m);
 
-/// Computes <tt>a + b</tt> modulo m. Operand @c a must already be in the range <tt>0 <= a < m</tt>, and operand @c b
-/// in the range <tt>0 <= b <= m</tt> (note that @c b is allowed to be equal to @c m).
+/// Computes <tt>a + b</tt> modulo m.
 ///
-/// @tparam IntType The type of @c a, @c b and @c m.
-/// @param a The first operand.
-/// @param a The second operand.
-/// @return The unique integer modulo @c m which is congruent to <tt>a + b</tt>
+/// @pre @c a must already be in the range <tt>0 <= a < m</tt>, and operand @c b in the range <tt>0 <= b <= m</tt> (note
+/// that @c b is allowed to be equal to @c m).
 template <class IntType>
 IntType add_modulo(IntType a, IntType b, IntType m);
 
-/// Computes <tt>a - b</tt> modulo m. Operand @c a must already be in the range <tt>0 <= a < m</tt>, and operand @c b
-/// in the range <tt>0 <= b <= m</tt> (note that @c b is allowed to be equal to @c m).
+/// Computes <tt>a - b</tt> modulo m.
 ///
-/// @tparam IntType The type of @c a, @c b and @c m.
-/// @param a The first operand.
-/// @param a The second operand.
-/// @return The unique integer modulo @c m which is congruent to <tt>a - b</tt>
+/// @pre @c a must already be in the range <tt>0 <= a < m</tt>, and operand @c b in the range <tt>0 <= b <= m</tt> (note
+/// that @c b is allowed to be equal to @c m).
 template <class IntType>
 IntType sub_modulo(IntType a, IntType b, IntType m);
 
 /// Returns whether @c a comes before @c b when iterating over a cyclic set starting from @c base.
-///
-/// @tparam T The type of elements to compare.
-/// @param base The base element.
-/// @param b The first operand.
-/// @param c The second operand.
-/// @return True iff @c a comes before @c b.
 template <class T>
 bool cyclic_less_than(const T& base, const T& a, const T& b);
 
